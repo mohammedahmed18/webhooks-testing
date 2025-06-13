8665183e16f1bacd0d02d0c99b72773b43e34ba9
mohammed <mohammed18200118@gmail.com>
2025-06-13 21:34:17 +0300

    added newFunc

--- app/main.py
+++ app/main.py
@@ -8,3 +8,8 @@ def sorter(arr):
                 arr[j + 1] = temp
     print(f"result: {arr}")
     return arr
+
+
+def newFunc():
+    print("codeflash stdout: This is a new function")
+    return "This is a new function"
