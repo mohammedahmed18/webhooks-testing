5254faed857f19ba01a307dc28c362f4bab1e3c9
mohammed <mohammed18200118@gmail.com>
2025-07-02 15:37:18 +0300

    remove some method

--- app/main.py
+++ app/main.py
@@ -10,12 +10,21 @@ def sorter(arr):
     return arr
 
 
+# def find_common_tags(articles: list[dict[str, list[str]]]) -> set[str]:
+#     """
+#     example articles: [
+#         {
+#             "tags": ["backend", "security"]
+#         },
+#         {
+#             "tags": ["llm", "security"]
+#         }
+#     ]
+#     """
+#     if not articles:
+#         return set()
 
-def find_common_tags(articles: list[dict[str, list[str]]]) -> set[str]:
-    if not articles:
-        return set()
-
-    common_tags = articles[0]["tags"]
-    for article in articles[1:]:
-        common_tags = [tag for tag in common_tags if tag in article["tags"]]
-    return set(common_tags)
+#     common_tags = articles[0]["tags"]
+#     for article in articles[1:]:
+#         common_tags = [tag for tag in common_tags if tag in article["tags"]]
+#     return set(common_tags)
