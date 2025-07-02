f8c4c2fc10d1de305544c95d1a054c71d4233df4
mohammed <mohammed18200118@gmail.com>
2025-07-02 15:54:20 +0300

    refactor

--- app/main.py
+++ app/main.py
@@ -3,28 +3,23 @@ def sorter_lts(arr):
     for i in range(len(arr)):
         for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+                swap(arr[j], arr[j + 1])
     print(f"result: {arr}")
     return arr
 
 
-# def find_common_tags(articles: list[dict[str, list[str]]]) -> set[str]:
-#     """
-#     example articles: [
-#         {
-#             "tags": ["backend", "security"]
-#         },
-#         {
-#             "tags": ["llm", "security"]
-#         }
-#     ]
-#     """
-#     if not articles:
-#         return set()
+def swap(val1, val2):
+    temp = val1
+    val1 = val2
+    val2 = temp
+    return (val1, val2)
 
-#     common_tags = articles[0]["tags"]
-#     for article in articles[1:]:
-#         common_tags = [tag for tag in common_tags if tag in article["tags"]]
-#     return set(common_tags)
+def sorter_lts2(arr):
+    print("codeflash stdout: Sorting list")
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                swap(arr[j], arr[j + 1])
+    print(f"result: {arr}")
+    return arr
+
