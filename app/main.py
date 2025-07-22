13dc1a90dffedc8bd0841e9f6274f826104d4101
mohammed <mohammed18200118@gmail.com>
2025-07-22 20:08:32 +0300

    change

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from app.utils import swap
 
-def sorter_test_final4(arr):
+def sorter_test_final5(arr):
     print("codeflash stdout: Sorting list")
     for i in range(len(arr)):
         for j in range(len(arr) - 1):
