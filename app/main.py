0718ed95522bf38accbb9590c82fc0e002c04fe2
mohammed <mohammed18200118@gmail.com>
2025-08-01 14:45:26 +0300

    format

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from app.utils import swap
 
+
 def sorter_test_final10(arr):
     print("codeflash stdout: Sorting list")
     for i in range(len(arr)):
         for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
-                arr[j] , arr[j + 1] = swap(arr[j], arr[j + 1])
+                arr[j], arr[j + 1] = swap(arr[j], arr[j + 1])
     print(f"result: {arr}")
     return arr
 
@@ -15,7 +16,6 @@ def sorter_lts2(arr):
     for i in range(len(arr)):
         for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
-                arr[j] , arr[j + 1] = swap(arr[j], arr[j + 1])
+                arr[j], arr[j + 1] = swap(arr[j], arr[j + 1])
     print(f"result: {arr}")
     return arr
-
