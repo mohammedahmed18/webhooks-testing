69d3d2ddff0cf67f800f8647621bd76a867e2485
mohammed <mohammed18200118@gmail.com>
2025-08-14 18:10:52 +0300

    new

--- app/main.py
+++ app/main.py
@@ -11,11 +11,13 @@ def sorter_test_final10(arr):
     return arr
 
 
-def sorter_lts2(arr):
+def sorter_new(arr):
     print("codeflash stdout: Sorting list")
     for i in range(len(arr)):
         for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = swap(arr[j], arr[j + 1])
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
     print(f"result: {arr}")
     return arr
