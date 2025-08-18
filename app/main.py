fa24ff5aeea5cfcd8e152436931630611a58208e
ali <mohammed18200118@gmail.com>
2025-08-18 23:16:48 +0300

    changes

--- app/main.py
+++ app/main.py
@@ -1,18 +1,3 @@
-from app.utils import swap
-
-
-def sorter_test_final10(arr):
-    print("codeflash stdout: Sorting list")
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = swap(arr[j], arr[j + 1])
-    print(f"result: {arr}")
-    return arr
-
-
-
-
 def another_function():
     print("Hello world")
 
