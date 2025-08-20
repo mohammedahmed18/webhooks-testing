16efdaaef93f96db9c798a7bd0c426934f8428f6
ali <mohammed18200118@gmail.com>
2025-08-20 16:43:56 +0300

    temp

--- app/main.py
+++ app/main.py
@@ -3,14 +3,9 @@ def another_function():
 
 
 # temp 2
-def sorter(arr):
+def sorter2(arr):
     # this is a modified version of the sorter
     print("codeflash stdout: Sorting list")
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
-            if arr[j] > arr[j + 1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+    arr.sort()
     print(f"result: {arr}")
     return arr
