29eca391a397806254637a6f93788bbd64f59362
ali <mohammed18200118@gmail.com>
2025-08-28 00:58:17 +0300

    commit bro

--- app/main.py
+++ app/main.py
@@ -10,8 +10,11 @@ def bubble_sort(arr):
 def bubble_sort5(arr):
     n = len(arr)
     for i in range(n):
+        swapped = False
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
     return arr
-    
--- app/utils.py
+++ app/utils.py
@@ -12,7 +12,7 @@ def sorter(arr):
 
 def sorter2(arr):
     for i in range(len(arr)):
-        for j in range(len(arr) - 1):
+        for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = swap2(arr[j], arr[j + 1])
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
