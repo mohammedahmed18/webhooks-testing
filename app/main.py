6ea822104cb50b5c5a3f25ca9727e2fd90a1c143
ali <mohammed18200118@gmail.com>
2025-08-29 02:18:25 +0300

    commit bro

--- app/main.py
+++ app/main.py
@@ -24,9 +24,12 @@ def bubble_sort_new(arr):
     n = len(arr)
     for i in range(n):
         swapped = False
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        bound = n - i - 1
+        for j in range(bound):
+            a = arr[j]
+            b = arr[j + 1]
+            if a > b:
+                arr[j], arr[j + 1] = b, a
                 swapped = True
         if not swapped:
             break
