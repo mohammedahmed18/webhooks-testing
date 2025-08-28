261f1889c512ea3fff8423c8b4743bf12b980996
ali <mohammed18200118@gmail.com>
2025-08-28 15:00:14 +0300

    new commit

--- app/main.py
+++ app/main.py
@@ -18,3 +18,16 @@ def bubble_sort5(arr):
         if not swapped:
             break
     return arr
+
+
+def bubble_sort_new(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
