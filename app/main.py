8cc577c9eb70f78e99ece2e6d679c3085628cd01
ali <mohammed18200118@gmail.com>
2025-08-29 19:55:22 +0300

    test

--- app/main.py
+++ app/main.py
@@ -7,18 +7,18 @@ def bubble_sort(arr):
     return arr
 
 
-def bubble_sort_4(arr1):
-    n = len(arr1)
+def bubble_sort_4(arr):
+    n = len(arr)
     for i in range(n):
         did_swapped = False
         for j in range(0, n - i - 1):
-            if arr1[j] > arr1[j + 1]:
-                arr1[j], arr1[j + 1] = arr1[j + 1], arr1[j]
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 did_swapped = True
         if not did_swapped:
             break
 
-    return arr1
+    return arr
 
 
 
