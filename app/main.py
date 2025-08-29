00b0d4d75226a215ca857b5e2f5891b127c2406a
ali <mohammed18200118@gmail.com>
2025-08-29 14:33:48 +0300

    test

--- app/main.py
+++ app/main.py
@@ -10,12 +10,12 @@ def bubble_sort(arr):
 def bubble_sort_1(arr):
     n = len(arr)
     for i in range(n):
-        swapped = False
+        did_swapped = False
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        if not swapped:
+                did_swapped = True
+        if not did_swapped:
             break
 
     return arr
