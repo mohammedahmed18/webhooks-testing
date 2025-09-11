34c408376098c5ca8b28d75ce81396e9f2771d24
ali <mohammed18200118@gmail.com>
2025-09-11 15:11:00 +0300

    temp

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
+def sorter(arr):
+    print("codeflash stdout: Sorting list")
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    print(f"result: {arr}")
     return arr
 
 
